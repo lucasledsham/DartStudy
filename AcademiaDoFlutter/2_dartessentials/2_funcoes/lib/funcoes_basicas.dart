d44ef5f2feafbef9bb340dfa0189db9fea75e194
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-07-17 18:02:37 -0300

    Função também é um tipo

--- AcademiaDoFlutter/2_dartessentials/2_funcoes/lib/funcoes_basicas.dart
+++ AcademiaDoFlutter/2_dartessentials/2_funcoes/lib/funcoes_basicas.dart
@@ -1,6 +1,16 @@
-void main() {}
+// ignore_for_file: unused_local_variable, unused_element
+void main() {
+  recuperarIdade(); // Invocando a funcao
+  var idade = idadeFuncao();
+  var funcaoRecuperarIdade =
+      recuperarIdade; // Atribuindo uma funcao para uma variavel
+}
 
 // TODA FUNCAO DEVE RETORNAR UM VALOR, NUNCA DEVE SER DEIXADO VAZIO
-int recuperarIdade() {
-  return 42;
+void recuperarIdade() {
+  print('Executando...');
+}
+
+int idadeFuncao() {
+  return 5;
 }
