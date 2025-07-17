b8f7e50318f1290968116fa15bd7fcec3597aacc
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-07-17 15:56:43 -0300

    Inferencia de tipos

---
+++ AcademiaDoFlutter/dartessentials/lib/aula/tipo_declaracao_variaveis.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_local_variable, unused_element
+
+void main() {
+  // Declaracao das variaveis
+  // CTRL + ALT para ver os tipos "escondidos" com o var
+  int idade = 30;
+  var idadeComVar = 30;
+
+  var idadeMias1 = idadeComVar + 1;
+}
