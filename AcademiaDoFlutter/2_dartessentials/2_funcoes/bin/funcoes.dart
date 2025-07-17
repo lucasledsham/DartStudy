a3c4d15879c9cc2ac7445aeab64226f5714fd5d5
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-07-17 17:51:37 -0300

    Aula Anatomia de uma função

--- AcademiaDoFlutter/2_dartessentials/2_funcoes/bin/funcoes.dart
+++ AcademiaDoFlutter/2_dartessentials/2_funcoes/bin/funcoes.dart
@@ -1,5 +1,1 @@
-import 'package:funcoes/funcoes.dart' as funcoes;
-
-void main(List<String> arguments) {
-  print('Hello world: ${funcoes.calculate()}!');
-}
+void main(List<String> arguments) {}
---
+++ AcademiaDoFlutter/2_dartessentials/2_funcoes/lib/funcoes_basicas.dart
@@ -0,0 +1,6 @@
+void main() {}
+
+// TODA FUNCAO DEVE RETORNAR UM VALOR, NUNCA DEVE SER DEIXADO VAZIO
+int recuperarIdade() {
+  return 42;
+}
