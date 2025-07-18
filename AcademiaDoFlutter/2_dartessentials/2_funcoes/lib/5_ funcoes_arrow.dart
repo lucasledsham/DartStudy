e7f1ca9c63b2ae5b52f860de8170771e169913a8
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-07-18 16:05:11 -0300

    Funções Arrow

---
+++ AcademiaDoFlutter/2_dartessentials/2_funcoes/lib/5_ funcoes_arrow.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  var somar = (int a, int b) => a + b;
+}
+
+// Diminui o tamanho da funcao
+// Bom para utilizar em funcoes que dentro corpo so possui uma linha
+// Utilizar em funcoes muito simples
+int somar(int a, int b) => a + b;
