ee434d389c54cc99a1b39583bb95e38d43e8fc74
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-07-18 17:21:34 -0300

    Comentários de ações (TODO, FIXME, HACK)

---
+++ AcademiaDoFlutter/2_dartessentials/3_comentarios/lib/3_todo_fixme_hack.dart
@@ -0,0 +1,10 @@
+// Visualizar os TODO, FIXME e HACK
+// CTRL + SHIFT + P
+// View: Show TODOs
+
+// TODO: Implementar Funcionalidade Futura aqui!!
+// TODO: Falta fazer
+void exmeplo() {
+  // FIXME: Corrigir esse codigo
+  // HACK: Solucao temporaria para um problema especifico
+}
