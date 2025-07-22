08a5c0fae67c62f0f94559fc20c0bf366742f546
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-07-21 23:03:26 -0300

    Aula Null Assert Operator (Bang Operator)

---
+++ AcademiaDoFlutter/2_dartessentials/6_null_safety/lib/5_bang_operator.dart
@@ -0,0 +1,21 @@
+// Nao utiliar Bang operator (!) SEM FAZER UMA CHECAGEM DE NULO ANTES
+
+// ignore_for_file: file_names, unused_local_variable
+
+String? nomeGlobal;
+
+void imprimirNomeGlobal() {
+  if (nomeGlobal != null) {
+    // print((nomeGlobal as String).length);
+    print(nomeGlobal!.length);
+  }
+
+  String? mensagem = obterMensagem();
+  if (mensagem != null) {
+    print(mensagem.length);
+  }
+}
+
+String? obterMensagem() {
+  return 'Mensagem do Lucas';
+}
