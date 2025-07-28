109bce412c1c6dc85b8338dc9cbd1d38bbe8c079
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-07-28 15:00:07 -0300

    Aula Construtores Privado

---
+++ AcademiaDoFlutter/3_orientacao_a_objetos/oop_dart/lib/2_construtorea/3_configuracao.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: file_names
+
+class Configuracao {
+  static String token = 'abc';
+  Configuracao._();
+}
---
+++ AcademiaDoFlutter/3_orientacao_a_objetos/oop_dart/lib/2_construtorea/3_construtor_privao.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: file_names, unused_local_variable
+
+import 'package:oop_dart/2_construtorea/3_configuracao.dart';
+
+void main() {
+  // final config = Configuracao();
+
+  print(Configuracao.token);
+}
