ef8e0ea65a334de9b1ba1139bd2c5536ae34802a
Lucas Ledsham <lucasnunesleal@gmail.com>
2026-01-29 22:05:43 -0300

    TIpos Dart

---
+++ AcademiaDoFlutter/Dart Essentials/01_tipos/lib/01_tipos_existentes.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: unused_local_variable, unused_element
+
+void main() {
+  // Numeros: int, double
+  int idade = 26;
+  double salario = 1200.00;
+
+  // Separadores de números para melhor legibilidade
+  double salarioRony = 1_000_000.0;
+
+  // Strings
+  String nome = 'Lucas';
+
+  //Interpolação
+  String nomeCompleto = '$nome Ledsham'; // Lucas Ledsham
+}
