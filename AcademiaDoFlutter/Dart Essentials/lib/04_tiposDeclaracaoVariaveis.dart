334d41fc07a6d335c1c0912c51da9232ddaaf949
Lucas Ledsham <lucasnunesleal@gmail.com>
2026-01-31 22:58:54 -0300

    Variaveis com var

--- AcademiaDoFlutter/Dart Essentials/lib/04_tiposDeclaracaoVariaveis.dart
+++ AcademiaDoFlutter/Dart Essentials/lib/04_tiposDeclaracaoVariaveis.dart
@@ -1,3 +1,16 @@
 // ignore_for_file: unused_local_variable, unused_element
 
-void main() {}
+void main() {
+  // Declaração das variaveis
+  int idade = 8;
+  String nome = 'Lucas';
+  double preco = 500.50;
+  bool verdadeiro = true;
+
+  // Possível declarar apenas com var
+  // CTRL + ALT mostra coisas implicitas dentro do codigo
+  var idadeComVar = 60;
+  var booleanComVar = false;
+  var doubleComVar = 55.6;
+  var stringComVar = 'Pedro';
+}
