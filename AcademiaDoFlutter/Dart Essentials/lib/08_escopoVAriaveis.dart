48d3ff33204d344b13bffa48c2b4bfbddbf6ea79
Lucas Ledsham <lucasnunesleal@gmail.com>
2026-02-02 23:17:07 -0300

    Escopo Varíaveis

---
+++ AcademiaDoFlutter/Dart Essentials/lib/08_escopoVAriaveis.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: unused_local_variable, unused_element
+
+/*
+Tres Escopos
+1. Escopo GLobal
+2. Escopo de Função
+3. Escopo de Blobo
+*/
+
+// Varíavel Global (Varíavel de Nível Superior)
+// Pode ser acessado por todos
+var nome = 'Lucas';
+void main() {
+  //Escopo de Função
+  // Só é acessado dentro das chaves que foi criado
+  var nomeComplero = 'Lucas Nunes';
+}
