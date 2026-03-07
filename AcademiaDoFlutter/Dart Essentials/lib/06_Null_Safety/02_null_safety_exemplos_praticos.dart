232dde65d64bebd8722e130b13f39de9f2034f0e
Lucas Ledsham <lucasnunesleal@gmail.com>
2026-03-06 23:29:59 -0300

    Null Safety exemplo prático

---
+++ AcademiaDoFlutter/Dart Essentials/lib/06_Null_Safety/02_null_safety_exemplos_praticos.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: unused_local_variable, avoid_init_to_null, body_might_complete_normally_nullable
+
+// Função que irá receber uma String que pode ser null
+String? cadastrarUsuario({String? nome, String? email}) {
+  String texto = 'Não possui cadasto';
+  // Regra de Negócio todo o nome precisa estar em maiúsculo
+  // var nomeMaiusculo = nome.toUpperCase(); Erro em tempo de Complilação, por poder receber nulo não é possível utilizar o método
+
+  // NECESSÁRIO FAZER VERIFICAÇÃO
+  if (nome != null && email != null) {
+    var nomeMaiusculo = nome.toUpperCase();
+      texto = 'Usuário cadastrado com Sucesso';
+    } else {
+      texto = '$nomeMaiusculo não possui email vinculado';
+    }
+
+  }
+    return texto;
+}
