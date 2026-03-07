65b70a50343f73a8b0038accc528947c0052ba38
Lucas Ledsham <lucasnunesleal@gmail.com>
2026-03-06 23:06:20 -0300

    Null Safety Introdução

--- AcademiaDoFlutter/Dart Essentials/lib/06_Null_Safety/01_null_safety_introducao.dart
+++ AcademiaDoFlutter/Dart Essentials/lib/06_Null_Safety/01_null_safety_introducao.dart
@@ -7,10 +7,24 @@ void main() {
   String nome = 'Lucas';
   String nomeNull;
 
-  // Lista que pode conter elementos nulos
+  // Lista que não pode conter elementos nulos
   List<String> listaNaoNulaQueNaoAceitaNula = ['Rodrigo', 'Rahman'];
+  // Lista que pode conter elementos nulos
   List<String?> listaNaoNulaQueAceitaNula = ['Rodrigo', null, 'Rahman'];
 
   // Lista que pode ser Nula
+  List<String> listaQueNaoPodeSerNula = ['Lista não pode ser nula'];
+  // Lista que não pode ser Nula
   List<String>? listaQuePodeSerNula = null;
+
+  // No Map é possível deixar apenas a chave ou apenas o valor ou ambos para receberem valores nulos
+  // Map que não pode conter elementos nulos
+  Map<String, String> elementosMapNaoNulo = {'Chave 1': 'Valor 1'};
+  // Map que pode conter elementos nulos
+  Map<String?, String?> elementosMapPodeNulo = {'Chave 1': null};
+
+  // Map que não pode ser nulo
+  Map<String, String> mapNaoNulo = {'Chave 1': 'Valor 1'};
+  // Map que pode ser nulo
+  Map<String, String>? mapPodeSerNulo = null;
 }
